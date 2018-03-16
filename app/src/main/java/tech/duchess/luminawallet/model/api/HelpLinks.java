403cc9b473c9ae7c02126eada1f7be861f84e45b
Timothy Duch <timothyduch@duchess.tech>
2018-03-16 00:58:16 -0500

    Adding terms and conditions of use

--- app/src/main/java/tech/duchess/luminawallet/model/api/HelpLinks.java
+++ app/src/main/java/tech/duchess/luminawallet/model/api/HelpLinks.java
@@ -9,6 +9,8 @@ public class HelpLinks {
             "https://www.stellar.org/developers/guides/concepts/inflation.html";
     public static final String LUMENAUT =
             "https://lumenaut.net";
+    public static final String TERMS_AND_CONDITIONS =
+            "https://gitlab.com/duchesstech/LuminaWallet/wikis/Terms-and-Conditions-of-Use";
     public static final String PRIVACY_POLICY =
             "https://gitlab.com/duchesstech/LuminaWallet/wikis/Lumina-Privacy-Policy";
     public static final String SOURCE_CODE =
